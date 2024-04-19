b7bfcfe9a223165cbe04153cee9ff817b5529539
mau0 <mau0mv2@gmail.com>
2024-04-19 13:40:06 -0600

    ci: :building_construction: config pwa

--- Rex/vite.config.ts
+++ Rex/vite.config.ts
@@ -1,7 +1,44 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import { VitePWA } from "vite-plugin-pwa";
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "My PWA",
+        short_name: "PWA",
+        start_url: ".",
+        display: "standalone",
+        background_color: "#ffffff",
+        theme_color: "#000000",
+        description: "My awesome PWA!",
+        icons: [
+          {
+            src: "path-to-icon.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+        ],
+      },
+      workbox: {
+        // workbox options for generateSW
+        runtimeCaching: [
+          {
+            urlPattern: /^https?.*/,
+            handler: "NetworkFirst",
+            options: {
+              cacheName: "https-calls",
+              expiration: {
+                maxEntries: 150,
+                maxAgeSeconds: 30 * 24 * 60 * 60, // 1 month
+              },
+              networkTimeoutSeconds: 15,
+            },
+          },
+        ],
+      },
+    }),
+  ],
+});
