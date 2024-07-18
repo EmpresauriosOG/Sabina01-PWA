0cd451039d4d549efb0b81af4c3a0cbebb373966
mau0 <mau0mv2@gmail.com>
2024-07-18 17:12:39 -0600

    feat: :sparkles: some hooks

---
+++ Rex/src/utils/ingredientUtils.ts
@@ -0,0 +1,43 @@
+import { Ingredient } from "@/components/tables/Ingredients/types";
+import axios from "axios";
+
+interface IngredientsResponse {
+  ingredients: Ingredient[];
+}
+
+export interface NoStaffFoundError {
+  detail: string;
+}
+
+export const fetchIngredients = async (restaurant_id: string) => {
+  const options = {
+    method: "GET",
+    url: `https://sabina01.onrender.com/ingredients/${restaurant_id}`,
+  };
+  try {
+    const response = await axios.request(options);
+    console.log(response);
+    return response.data as IngredientsResponse;
+  } catch (error) {
+    console.error("Oops", error);
+    return { ingredients: [] };
+  }
+};
+
+export const submitIngredient = async (data: Ingredient) => {
+  const options = {
+    method: "POST",
+    url: "https://sabina01.onrender.com/ingredients/upload-new-ingredient",
+    data,
+  };
+  try {
+    const response = await axios.request(options);
+    console.log(data);
+    console.log(response.data);
+    return response.data;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (error: any) {
+    console.error("Oops");
+    throw new Error(error);
+  }
+};
