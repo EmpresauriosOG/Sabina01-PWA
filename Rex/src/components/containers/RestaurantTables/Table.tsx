37d8c4519854109e82e70e828f0a0318c3432b64
mau0 <mau0mv2@gmail.com>
2024-10-14 17:52:47 -0600

    refactor: :recycle: refactor tables design

--- Rex/src/components/containers/RestaurantTables/Table.tsx
+++ Rex/src/components/containers/RestaurantTables/Table.tsx
@@ -14,24 +14,24 @@ const DisplayedTable: React.FC<TableProps> = ({
   isSelected,
 }) => {
   const statusColors: { [key: number]: string } = {
-    0: "bg-green-500",
-    1: "bg-red-500",
-    2: "bg-yellow-500",
+    0: "border-green-500",
+    1: "border-red-500",
+    2: "border-yellow-500",
   };
 
   return (
     <div
       className={cn(
-        "w-24 h-24 m-2 rounded-lg flex flex-col items-center justify-center cursor-pointer transition-all",
+        "w-full max-w-xs p-4 m-2 rounded-lg flex flex-col items-center justify-center cursor-pointer transition-all border-4",
         statusColors[table.status],
         isSelected && "ring-4 ring-blue-500"
       )}
       onClick={onClick}
     >
-      <span className="font-bold text-white">Mesa {table.table_number}</span>
-      <span className="text-sm text-white">
-        {table.number_of_persons} Personas
-      </span>
+      <h2 className=" text-lg text-center mb-2">Mesa {table.table_number}</h2>
+      <span className="text-sm">{table.number_of_persons} Personas</span>
+      <span className="text-sm">Status: {table.status}</span>
+      <span className="text-sm">Mesero: {table.current_waiter}</span>
     </div>
   );
 };
